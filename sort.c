a66dd35aaa259537bbe4045f61078f0739f7aff7
Leshin Artem <lesinrtm@gmail.com>
2025-11-29 15:18:01 +0700

    Изменил файл sort.c для выполнения задания на 4

--- sort.c
+++ sort.c
@@ -11,16 +11,3 @@ int compare_ints(const void* a, const void* b) {
     return 0;
 }
 
-int main() {
-    int numbers[] = {5, 2, 8, 1, 9};
-    size_t numbers_size = sizeof(numbers) / sizeof(numbers[0]);
-
-    qsort(numbers, numbers_size, sizeof(int), compare_ints);
-
-    for (size_t i = 0; i < numbers_size; ++i) {
-        printf("%d ", numbers[i]);
-    }
-    
-
-    return 0;
-}
