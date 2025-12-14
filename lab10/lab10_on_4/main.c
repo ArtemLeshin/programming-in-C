3f252d535e3585cff054ef82929fcd3b813da69d
Leshin Artem <lesinrtm@gmail.com>
2025-12-14 23:47:42 +0700

    Добавил лабу 10 на 5

---
+++ lab10/lab10_on_4/main.c
@@ -0,0 +1,21 @@
+#include "fraction.h"
+int main() {
+    printf("Напишите числитель и знаменатель первой дроби через пробел\n");
+    int a,b;
+    scanf("%d %d", &a,&b);
+    printf("Напишите числитель и знаменатель второй дроби через пробел\n");
+    int a1,b1;
+    scanf("%d %d", &a1,&b1);
+    struct fraction *fraction1 = init(a,b);
+    struct fraction *fraction2 = init(a1,b1);
+    printf("Первая дробь: ");
+    print_func(fraction1);
+    printf("Вторая дробь: ");
+    print_func(fraction2);
+    struct fraction *multi= multiplication(fraction1,fraction2);
+    printf("Умножение дробей: ");
+    print_func(multi);
+    struct fraction *div= division(fraction1,fraction2);
+    printf("Деление дробь: ");
+    print_func(div);
+}
